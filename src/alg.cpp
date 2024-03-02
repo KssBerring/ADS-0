adab77600490c80a83d540923ead11b747f00d9a
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-03-02 20:23:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,4 +2,10 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
+    while (b != 0) {
+        int tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
 }
